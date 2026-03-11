7b9f696c7718d833ff7d7c3550fbb78a510acc94
jeevi2511 <js3382@srmist.edu.in>
2026-03-11 15:08:53 +0530

    Use Case 1

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,31 @@
+/**
+ * HotelBookingApplication
+ *
+ * This class represents the entry point of the Hotel Booking application.
+ * It demonstrates how a Java program begins execution and produces
+ * console output.
+ *
+ * The application prints a welcome message along with the
+ * application name and version information.
+ *
+ * @author Arieve
+ * @version 1.0
+ */
+public class HotelBookingApplication {
+
+    /**
+     * The main method is the starting point of the Java application.
+     * The Java Virtual Machine (JVM) calls this method when the
+     * program is executed.
+     *
+     * @param args command line arguments passed to the application
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the Hotel Booking System!");
+        System.out.println("Application: Hotel Booking System");
+        System.out.println("Version: v1.0");
+
+        System.out.println("Application started successfully.");
+    }
+}
